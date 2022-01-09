b7ad3d725586ae7ca90d6b11ed82759a7cd25bcb
Douglas Duarte <douglasg3x@hotmail.com>
2022-01-09 17:37:17 +0000

    redirect endipoint

--- src/main/java/projectmanager/ProjectManager.java
+++ src/main/java/projectmanager/ProjectManager.java
@@ -2,10 +2,22 @@ package projectmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.result.view.RedirectView;
+
 
 @SpringBootApplication()
+@Controller
+@RequestMapping(path = "/")
 public class ProjectManager {
+
+
+	@GetMapping
+    public RedirectView listProjects() {
+        return new RedirectView("project");
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManager.class, args);
